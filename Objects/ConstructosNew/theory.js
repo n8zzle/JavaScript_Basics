28dda5d8d0f3205e0cb1d1b9b0b3399c66e6668c
Игорь Чичкаленко <iitz4i4a@Igors-MBP.fritz.box>
2024-10-14 00:28:03 +0200

    starting to work with constructor

--- Objects/ConstructosNew/theory.js
+++ Objects/ConstructosNew/theory.js
@@ -1,1 +1,12 @@
-https://learn.javascript.ru/constructor-new
+//learn.javascript.ru/constructor-new
+
+//Constructor function
+https: function User(name) {
+  this.name = name;
+  this.isAdmin = false;
+}
+//When we are calling constructor funciton we always should use *NEW*
+const user = new User("Jack");
+
+console.log(user.name);
+console.log(user.isAdmin);
